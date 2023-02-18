8d649cf6ba34d14711f284de120e85c2dbf22ae6
Grégory <gregory.hue135@gmail.com>
2023-02-18 14:56:34 +0100

    First push to deploy on Netlify

--- front/remix.config.js
+++ front/remix.config.js
@@ -1,7 +1,12 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
+/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  cacheDirectory: "./node_modules/.cache/remix",
-  ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  ignoredRouteFiles: ["**/.*"],
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? "./server.js"
+      : undefined,
+  serverBuildPath: ".netlify/functions-internal/server.js",
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
 };
