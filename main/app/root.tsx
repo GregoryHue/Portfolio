7ddaeec353a8f0fd8a075c90258ce8ac7c384e28
Grégory <gregory.hue135@gmail.com>
2023-03-11 16:23:35 +0100

    Adding i18n

--- main/app/root.tsx
+++ main/app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import { json, LinksFunction, LoaderArgs, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -6,8 +6,28 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 import styles from '~/css/index.css'
+import { useTranslation } from "react-i18next";
+import i18next from "~/i18next.server";
+import { useEffect } from "react";
+
+export function useChangeLanguage(locale: string) {
+  let { i18n } = useTranslation();
+  useEffect(() => {
+    i18n.changeLanguage(locale);
+  }, [locale, i18n]);
+}
+
+export async function loader({ request }: LoaderArgs) {
+  let locale = await i18next.getLocale(request);
+  return json({ locale });
+}
+
+export let handle = {
+  i18n: "common",
+};
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
 
@@ -18,8 +38,13 @@ export const meta: MetaFunction = () => ({
 });
 
 export default function App() {
+  let { locale } = useLoaderData<typeof loader>();
+
+  let { i18n } = useTranslation();
+
+  useChangeLanguage(locale);
   return (
-    <html lang="en">
+    <html lang={locale} dir={i18n.dir()}>
       <head>
         <Meta />
         <Links />
--- main/app/routes/_index.tsx
+++ main/app/routes/_index.tsx
@@ -2,6 +2,7 @@ import { prisma } from '~/utils/prisma.server'
 import { useLoaderData } from '@remix-run/react'
 import { json, type LoaderFunction } from '@remix-run/node'
 import {Button, Typography} from '@mui/material'
+import { useTranslation } from "react-i18next";
 
 export const loader: LoaderFunction = async () => {
   const techno = await prisma.text.findFirst({
@@ -13,6 +14,8 @@ export const loader: LoaderFunction = async () => {
 
 export default function Index() {
   const { techno } = useLoaderData()
+  let { t } = useTranslation()
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
@@ -24,13 +27,7 @@ export default function Index() {
           </Typography>
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
+            <p>{t('home.text')}</p>
         </li>
         <li>
           <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
