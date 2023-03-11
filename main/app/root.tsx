d34794f1b9addb9a3a414d17d2614481384b4afc
Grégory <gregory.hue135@gmail.com>
2023-03-11 16:05:49 +0100

    Adding Sass

--- main/app/root.tsx
+++ main/app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,6 +7,9 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from '~/css/index.css'
+
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -26,6 +29,9 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+        <div className="stars"></div>
+        <div className="stars2"></div>
+        <div className="stars3"></div>
       </body>
     </html>
   );
