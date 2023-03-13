c61e88c01deb1801674c8e2a482007e04d54ccfb
Grégory <gregory.hue135@gmail.com>
2023-03-13 19:23:12 +0100

    Testing i18n with Remix again

--- main/app/routes/_index.tsx
+++ main/app/routes/_index.tsx
@@ -2,6 +2,7 @@ import { prisma } from '~/utils/prisma.server'
 import { useLoaderData } from '@remix-run/react'
 import { json, type LoaderFunction } from '@remix-run/node'
 import {Button, Typography} from '@mui/material'
+import { useTranslation } from "react-i18next";
 
 export const loader: LoaderFunction = async () => {
   const techno = await prisma.text.findFirst({
@@ -13,10 +14,11 @@ export const loader: LoaderFunction = async () => {
 
 export default function Index() {
   const { techno } = useLoaderData()
+  let { t } = useTranslation();
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>{t('navbar.home')} to Remix</h1>
       <ul>
         <li>
           <Typography
